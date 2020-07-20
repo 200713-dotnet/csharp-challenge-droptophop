7bd9045d556d647eb8e554827a0d450cc7c63c34
Harold Hopson <hdhopson88@yahoo.com>
2020-07-20 14:04:54 -0700

    add test

--- Palindrome.Testing/PalidromeTest.cs
+++ Palindrome.Testing/PalidromeTest.cs
@@ -1,14 +1,18 @@
 using System;
 using Xunit;
+using Palindrome.Domain;
 
 namespace Palindrome.Testing
 {
     public class PalindromeTest
     {
         [Fact]
-        public void Test1()
+        public void IsAPalindromeTest()
         {
+            var p = new Palindrome();
+            var answer = p.IsAPalindrome("racecar");
 
+            AssertTrue(answer);
         }
     }
 }
